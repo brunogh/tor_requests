d156fb2a9f10ace96a028458c0800741275a686c
Kyle Foster <kyle.b.foster@gmail.com>
2013-02-22 14:24:20 -0800

    Added POST Method

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -28,4 +28,29 @@ describe "Http" do
     
   end
 
+  describe "post" do
+    
+    context "with invalid parameters" do
+      it "does not work" do
+        expect { Tor::HTTP.post("google.com") }.to raise_error
+      end
+    end
+    
+    context "with URI parameter" do
+      it "works" do
+        res = Tor::HTTP.post(URI('http://posttestserver.com/post.php?dir=example'), {"q" => "query", "var" => "variable"})
+        res.code.should eq("200")
+      end
+    end
+    
+    context "with host, path and port parameters" do
+      it "works" do
+        res = Tor::HTTP.post('posttestserver.com', {"q" => "query", "var" => "variable"}, '/post.php?dir=example', 80)
+        res.code.should eq("200")
+      end
+      
+    end
+    
+  end
+
 end
