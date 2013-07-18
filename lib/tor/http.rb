eeb24b0ab4e41c67c1e9e9a42009ac73f2b29771
Stephen Blackstone <sblackstone@gmail.com>
2013-07-18 16:24:00 -0400

    Add the ability to change arbitrary headers

--- lib/tor/http.rb
+++ lib/tor/http.rb
@@ -15,7 +15,12 @@ module Tor
         port = uri_or_host.port
       end
       Net::HTTP.SOCKSProxy(Tor.configuration.ip, Tor.configuration.port).start(host, port) do |http|
-        res = http.get(path || uri_or_host.path)
+        request = Net::HTTP::Get.new(path || uri_or_host.path)
+        Tor.configuration.headers.each do |header, value|
+          request.add_field(header, value)
+        end
+        
+        res = http.request(request);
       end
       res
     end
@@ -34,6 +39,9 @@ module Tor
       Net::HTTP.SOCKSProxy(Tor.configuration.ip, Tor.configuration.port).start(host, port) do |http|
         request = Net::HTTP::Post.new(path)
         request.set_form_data(post_options)
+        Tor.configuration.headers.each do |header, value|
+          request.add_field(header, value)
+        end
         res = http.request(request)
       end
       res
