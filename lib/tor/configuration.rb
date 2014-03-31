f5a7e5364bc6402d2d8d22932eef2e41d9a8648c
Razvan Secara <razvan.secara@softvision.ro>
2014-03-31 10:23:55 +0300

    added support hor https requests

--- lib/tor/configuration.rb
+++ lib/tor/configuration.rb
@@ -7,19 +7,19 @@ module Tor
 
     attr_accessor :ip,
                   :port
-    
+
     attr_reader :headers
-    
+
     def add_header(header, value)
       @headers[header] = value
     end
-    
+
     def initialize
-      @ip = '127.0.0.1'
-      @port = 9050
+      @ip      = '127.0.0.1'
+      @port    = 9050
       @headers = Hash.new
-    end    
-    
+    end
+
   end
 
 end
